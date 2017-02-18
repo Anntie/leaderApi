ad54756b749d969343acb7937334a9447b5e4c89
Andrew <andrew.golovenko@gmail.com>
2017-02-18 13:25:59 +0200

    Initial commit

---
+++ app/config.php
@@ -0,0 +1,14 @@
+<?php
+	// ** Database settings ** //
+	/** DB Host */
+	define("DB_HOST", "localhost");
+
+	/** DB User */
+	define("DB_USER", "root");
+
+	/** DB Password */
+	define("DB_PASSWORD", "19FylhbR99");
+
+	/** DB Name */
+	define("DB_NAME", "leader");
+?>
---
+++ public/index.php
@@ -0,0 +1,2 @@
+<?php
+	 $app = require "../app/app.php";
